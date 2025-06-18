c3fadc7ca3339cf73074ceedd732f553f73d99b1
晓言 <126128055+xiaoyan011016@users.noreply.github.com>
2025-06-18 08:04:37 +0800

    update src file from pakeplus

---
+++ src/assets/index-CGfcMIHl.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c,f as r}from"./index-BGwiQ_ZP.js";const n={};function o(t,f){return r(),c("div",null," file ")}const a=e(n,[["render",o]]);export{a as default};
