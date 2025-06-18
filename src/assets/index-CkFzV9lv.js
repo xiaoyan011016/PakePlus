263039ed4cf53506122f3bdf786e8577f2424e12
晓言 <126128055+xiaoyan011016@users.noreply.github.com>
2025-06-18 08:04:47 +0800

    update src file from pakeplus

---
+++ src/assets/index-CkFzV9lv.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c,f as n}from"./index-BGwiQ_ZP.js";const o={};function r(t,s){return n(),c("div",null," openAccess ")}const f=e(o,[["render",r]]);export{f as default};
