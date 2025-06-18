6b2f1b4db1cc27c88277c81bc104696a69086b6c
晓言 <126128055+xiaoyan011016@users.noreply.github.com>
2025-06-18 08:05:56 +0800

    update src file from pakeplus

---
+++ src/assets/index-DKS7oYO2.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as r,f as c}from"./index-BGwiQ_ZP.js";const o={};function n(t,s){return c(),r("div",null," formdes ")}const a=e(o,[["render",n]]);export{a as default};
